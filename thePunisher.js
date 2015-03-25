58bbf332b8e5b5e91513e34fd4f61fe425868747
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-03-25 21:32:07 +0800

    add additional rave features to text and changed name input to selector

--- thePunisher.js
+++ thePunisher.js
@@ -18,16 +18,40 @@ function punish() {
   document.getElementById('punishment').innerHTML = punishment;
 }
 
+// Added two extra colors
 function crazyBackground() {
-  var colors = ["green", "red", "blue"];
+  var colors = ["green", "red", "blue", 'orange', 'yellow'];
   var randomNumber = Math.floor(Math.random() * colors.length);
 
   document.bgColor = colors[randomNumber];
 }
 
+// Randomly changes the color and size of h2 tag on clicking rave
+function crazyText() {
+  var colors = ["green", "red", "blue", 'orange', 'yellow'];
+  var randomNumber = Math.floor(Math.random() * colors.length);
+
+  var sizes = ["xx-small","x-small","small,medium","large","x-large","xx-large"];
+  var secondRandomNumber = Math.floor(Math.random() * sizes.length);
+
+  document.getElementById('punishment-salutation').style.color = colors[randomNumber];
+  document.getElementById('punishment-salutation').style.fontSize = sizes[secondRandomNumber];
+}
+
+// Variables needed to be taken outside of function scope to use in new stop function below
+var crazy; 
+var crazyText;
+
 function raveWDI() {
-  crazy = setInterval(crazyBackground, 200);
+  crazy = setInterval(crazyBackground, 50);
+  crazyText = setInterval(crazyText, 50);
+}
+
+// New stop function on click of new stop button
+
+function stopRaveWDI() {
+  clearInterval(crazy);
+  clearInterval(crazyText);
 }
 
 // to Stop the RAVE, pls clear the interval timer
-// clearInterval(crazy);
