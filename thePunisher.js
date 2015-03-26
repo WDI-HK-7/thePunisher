e64783560770f4f8d56967f96066b8848c17ec03
Elia Sommerlad <Elia@Elias-MacBook-Pro.local>
2015-03-26 10:15:21 +0800

    punishreward

--- thePunisher.js
+++ thePunisher.js
@@ -1,10 +1,13 @@
 var punishments = [];
+var rewards = [];
 var crazy;
 
-punishments.push("Thou Shall refer to us Thy majesty");
+punishments.push("Thou Shall refer to us as Thy majesty");
 punishments.push("20 Pushups (except for xiao: 20 with someone else on top)");
 punishments.push("Bring coffee/beverages to everyone");
 punishments.push("Sing a song");
+punishments.push("Eat a pack of dried durian")
+
 
 function punish() {
   var randomPunishment = Math.floor(Math.random() * punishments.length);
@@ -18,15 +21,33 @@ function punish() {
   document.getElementById('punishment').innerHTML = punishment;
 }
 
+
+rewards.push("Free Massage");
+rewards.push("Free Lunch");
+
+
+function reward() {
+  var randomReward = Math.floor(Math.random() * rewards.length);
+  var reward = rewards[randomReward];
+
+  var name = document.getElementById('name').value ;
+
+  var salutation = document.getElementById('punishment-salutation')
+  salutation.innerText = "Hi " + name + "!!" + " Ready to be rewarded?"
+
+  document.getElementById('punishment').innerHTML = reward;
+}
+
+
 function crazyBackground() {
-  var colors = ["green", "red", "blue"];
+  var colors = ["green", "red", "blue", "yellow", "purple"];
   var randomNumber = Math.floor(Math.random() * colors.length);
 
   document.bgColor = colors[randomNumber];
 }
 
 function raveWDI() {
-  crazy = setInterval(crazyBackground, 200);
+  crazy = setInterval(crazyBackground, 800);
 }
 
 // to Stop the RAVE, pls clear the interval timer
