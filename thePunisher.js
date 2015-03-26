e7b1e710018c65764ac3b3e4addbe2701ca55503
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-03-26 10:11:52 +0800

    Michaels Music Button

--- thePunisher.js
+++ thePunisher.js
@@ -29,5 +29,51 @@ function raveWDI() {
   crazy = setInterval(crazyBackground, 200);
 }
 
+function pickASongWDI() {
+  var song = Math.round(Math.random()*100);
+
+  if (song  < 10) {
+    window.open('https://www.youtube.com/watch?v=oK19iX3L87w','_blank')
+  } else if (song < 20) {
+    window.open('https://www.youtube.com/watch?v=6_b7RDuLwcI','_blank')
+  } else if (song < 30) {
+    window.open('https://www.youtube.com/watch?v=vWHwSWkBjnY','_blank')
+  } else if (song < 40) {
+    window.open('https://www.youtube.com/watch?v=0FuL1VMWQ7Q#t=62','_blank')
+  } else if (song < 50) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=DCazMqCJVMc','_blank')
+  } else if (song < 60) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=SrkeWsQZNyU','_blank')
+  } else if (song < 70) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=K5G1FmU-ldg','_blank')
+  } else if (song < 80) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=sYffFEIAzdE','_blank')
+  } else if (song < 90) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=bRx0KNqKqnA','_blank')
+  } else if (song <=100) {
+    window.open('https://www.youtube.com/watch?feature=player_embedded&v=5ZYgIrqELFw','_blank')
+  }
+}
+
 // to Stop the RAVE, pls clear the interval timer
 // clearInterval(crazy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
