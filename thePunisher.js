3f7df87ac6a02da2012b536c60f218558222a2f5
Wing Man Li <victoriali@Wings-MacBook-Pro.local>
2015-03-26 10:13:17 +0800

    Victoria's punisher

--- thePunisher.js
+++ thePunisher.js
@@ -6,16 +6,34 @@ punishments.push("20 Pushups (except for xiao: 20 with someone else on top)");
 punishments.push("Bring coffee/beverages to everyone");
 punishments.push("Sing a song");
 
+
 function punish() {
   var randomPunishment = Math.floor(Math.random() * punishments.length);
   var punishment = punishments[randomPunishment];
 
   var name = document.getElementById('name').value ;
+  var numberTardies = document.getElementById('numberTardies').value ;
 
-  var salutation = document.getElementById('punishment-salutation')
-  salutation.innerText = "Hi " + name + "!!" + " Ready to be punished?"
+  if (name == "" || name == null){
+    alert('Please enter your name');
+    }
 
-  document.getElementById('punishment').innerHTML = punishment;
+  else{
+    if(numberTardies >= 5){
+      var salutation = document.getElementById('punishment-salutation');
+      salutation.innerText = "Hi " + name + "!!" + " Your Punishment is..."
+      document.getElementById('punishment').innerHTML = punishment;
+      var additional = document.getElementById('additional-punishment');
+      additional.innerText = "You also need to dye your hair!"
+      
+    }
+
+    else{
+      var salutation = document.getElementById('punishment-salutation')
+      salutation.innerText = "Hi " + name + "!!" + " Ready to be punished?"
+      document.getElementById('punishment').innerHTML = punishment;
+    } 
+  }
 }
 
 function crazyBackground() {
@@ -29,5 +47,9 @@ function raveWDI() {
   crazy = setInterval(crazyBackground, 200);
 }
 
-// to Stop the RAVE, pls clear the interval timer
-// clearInterval(crazy);
+function hairColour(){
+  var hairColour = ["green", "red", "blue", "pink", "brown", "orange"];
+  var randomHairNumber = Math.floor(Math.random() * hairColour.length);
+  document.getElementById('dye-hair-colour').innerHTML = hairColour[randomHairNumber];
+
+}
